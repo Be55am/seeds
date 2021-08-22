077a99beb2958c8996ac907d5ba7b33ba2fd931a
Bessam <ssahli@wissalgroup.com>
2021-08-22 21:53:30 +0100

    first running spring boot version

---
+++ back/core/src/main/java/com/goldminds/dto/TestDTO.java
@@ -0,0 +1,12 @@
+package com.goldminds.dto;
+
+import com.goldminds.common.dto.AbstractDTO;
+
+/**
+ * @author bessam on 22/08/2021
+ */
+
+public class TestDTO extends AbstractDTO {
+
+	String test;
+}
